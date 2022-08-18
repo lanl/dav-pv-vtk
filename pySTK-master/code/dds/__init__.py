c0ee45fd3d90e319083ee4549b0efc536dd8520f
Sonal Jha <sonalj@pn2003895.lanl.gov>
2022-08-18 13:46:56 -0600

    finalize gnn-assisted-sampling code

--- pySTK-master/code/dds/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dds import graph_based_sampling
-from .dds import graph
-from .dds import hist_based_sampling
--- pySTK-master/gnn/common.py
+++
@@ -1,7 +0,0 @@
-
-enum_dict = {
-    'rand': 0, 'hist': 1, 'hist_grad': 2, 'hist_grad_rand': 3, 'grad': 4, 'lcc': 5, 'max': 6, 'similarity': 7,
-    'max_neighbor': 8, 'random_walk': 9, 'value_weighted_random_walk': 10, 'similarity_weighted_random_walk': 11,
-    'max_seeded_random_walk': 12, 'max_seeded_weighted_random_walk': 13, 'complex_max_seeded_random_walk': 14,
-    'hist_lcc': 15, 'grad_lcc': 16, 'gnn': 17
-    }
